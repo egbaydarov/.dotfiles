aa1ce6007ab7121fc2d4b33c9a430e1df239193a
Egor Baydarov <ebaydarov@Egors-MacBook-Air-11109.local>
2025-02-09 18:27:11 +0300

    better color; line numbers

--- nvim/lua/byda/lazy/rose-pine.lua
+++ nvim/lua/byda/lazy/rose-pine.lua
@@ -2,6 +2,10 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     opts = {
+        variant = "dawn",
+        dark_variant = "dawn",
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
         highlight_groups = {
             TelescopeBorder = { fg = "overlay", bg = "overlay" },
             TelescopeNormal = { fg = "subtle", bg = "overlay" },
@@ -16,22 +20,33 @@ return {
             TelescopePromptNormal = { fg = "text", bg = "surface" },
             TelescopePromptBorder = { fg = "surface", bg = "surface" },
 
-            CurSearch = { fg = "base", bg = "leaf", inherit = false },
-            Search = { fg = "text", bg = "leaf", blend = 20, inherit = false },
+            CurSearch = { fg = "base", bg = "leaf", inherit = true },
+            Search = { fg = "text", bg = "leaf", blend = 20, inherit = true },
 
             StatusLine = { fg = "love", bg = "love", blend = 10 },
             StatusLineNC = { fg = "subtle", bg = "surface" },
-        }
+        },
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = false,
+        },
+        enable = {
+          terminal = true,
+          legacy_highlights = true,
+          migrations = true,
+        },
+        disable_background = true
     },
     config = function(_, opts)
         -- Set highlights if needed
         require("rose-pine").setup(opts)
 
-        -- Set colorscheme
         vim.cmd("colorscheme rose-pine-dawn")
 
         -- Status line configuration
         vim.opt.laststatus = 3 -- Global statusline
         vim.opt.statusline = " %f %m %= %l:%c ♥ "
+        vim.opt.pumblend = 0
     end
 }
