08dcdd038774f5d12c2bbd755108fd5833865983
egbaydarov <eg@byda.io>
2025-03-09 00:06:14 +0100

    fix lazy config

--- nvim/lua/byda/config/lazy.lua
+++ nvim/lua/byda/config/lazy.lua
@@ -18,68 +18,66 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup(
   {
-    {
-      "nvim-lua/plenary.nvim",
-      lazy = false,
-    },
-    {
-      "nvchad/base46",
-      lazy = false,
-      build = function()
-        require("base46").load_all_highlights()
-      end,
-    },
-    {
-      "nvchad/ui",
-      lazy = false,
-      config = function()
-        require "nvchad"
-      end,
-    },
-    {
-      "nvzone/volt",
-      lazy = false,
-    },
-    {
-      "nvzone/menu",
-      lazy = false,
-    },
-    {
-      "nvzone/minty",
-      lazy = false,
-      cmd = { "Huefy", "Shades" }
-    },
-    {
-      "nvim-tree/nvim-web-devicons",
-      lazy = false,
-      opts = function()
-        dofile(vim.g.base46_cache .. "devicons")
-        return { override = require "nvchad.icons.devicons" }
-      end,
-    },
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      lazy = false,
-      event = "User FilePost",
-      opts = {
-        indent = { char = "│", highlight = "IblChar" },
-        scope = { char = "│", highlight = "IblScopeChar" },
-      },
-      config = function(_, opts)
-        dofile(vim.g.base46_cache .. "blankline")
-
-        local hooks = require "ibl.hooks"
-        hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-        require("ibl").setup(opts)
-
-        dofile(vim.g.base46_cache .. "blankline")
-      end,
-    },
-  },
-  {
     root = vim.fn.stdpath("data") .. "/lazy", -- directory where plugins will be installed
     rocks = { enabled = false },
     spec = {
+      {
+        "nvim-lua/plenary.nvim",
+        lazy = false,
+      },
+      {
+        "nvchad/base46",
+        lazy = false,
+        build = function()
+          require("base46").load_all_highlights()
+        end,
+      },
+      {
+        "nvchad/ui",
+        lazy = false,
+        config = function()
+          require "nvchad"
+        end,
+      },
+      {
+        "nvzone/volt",
+        lazy = false,
+      },
+      {
+        "nvzone/menu",
+        lazy = false,
+      },
+      {
+        "nvzone/minty",
+        lazy = false,
+        cmd = { "Huefy", "Shades" }
+      },
+      {
+        "nvim-tree/nvim-web-devicons",
+        lazy = false,
+        opts = function()
+          dofile(vim.g.base46_cache .. "devicons")
+          return { override = require "nvchad.icons.devicons" }
+        end,
+      },
+      {
+        "lukas-reineke/indent-blankline.nvim",
+        lazy = false,
+        event = "User FilePost",
+        opts = {
+          indent = { char = "│", highlight = "IblChar" },
+          scope = { char = "│", highlight = "IblScopeChar" },
+        },
+        config = function(_, opts)
+          dofile(vim.g.base46_cache .. "blankline")
+
+          local hooks = require "ibl.hooks"
+          hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+          require("ibl").setup(opts)
+
+          dofile(vim.g.base46_cache .. "blankline")
+        end,
+      },
       { import = "byda.lazy" },
     },
     defaults = {
