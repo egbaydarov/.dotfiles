538ef51b4ffcfdf902e7b25c05c1f63178723aa0
egbaydarov <eg@byda.io>
2025-11-29 15:58:19 +0100

    clean up

---
+++ .config/waybar/vpn_p.sh
@@ -0,0 +1,1 @@
+sudo systemctl is-active --quiet openvpn-pVPN && echo '{"text":"","class":"on"}' || echo '{"text":"","class":"off"}'
---
+++ .config/waybar/vpn_wh.sh
@@ -0,0 +1,2 @@
+sudo systemctl is-active --quiet openvpn-wVPN && echo '{"text":"","class":"on"}' || echo '{"text":"","class":"off"}'
+
